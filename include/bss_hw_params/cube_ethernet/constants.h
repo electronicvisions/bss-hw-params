3a3f73809f137bbc3f3617aecc37aa9581f1d325
Robin Heinemann <robin.heinemann@kip.uni-heidelberg.de>
2026-01-28 16:04:47 +0100

    Increase for interrupt barrier removal
    Change-Id: I26ee809e9a18c73a8689bfcbb83c26aa5d0f0462

--- include/bss_hw_params/cube_ethernet/constants.h
+++ include/bss_hw_params/cube_ethernet/constants.h
@@ -7,7 +7,7 @@ namespace cube_ethernet {
  * FPGA bitfile compatibility.
  * Incremented on breaking changes within the FPGA design.
  */
-static constexpr size_t bitfile_compatible_until = 3;
+static constexpr size_t bitfile_compatible_until = 4;
 
 } // namespace cube_ethernet
 } // namespace bss_hw_params
